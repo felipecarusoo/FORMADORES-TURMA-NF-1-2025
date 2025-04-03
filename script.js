aa04bc40472bcf87e7faac67fc42a21374f4c4f4
Felipe Caruso <101845224+felipecarusoo@users.noreply.github.com>
2025-04-03 22:37:09 +0000

    .

--- script.js
+++ script.js
@@ -22,4 +22,12 @@ diminuiTamanhoBotao.addEventListener('click', function () {
     
     })
 
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+       botaoDeAcessibilidade.classList.toggle('rotacao-botao') 
+       opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+    })
 } )
